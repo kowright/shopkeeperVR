d54c01409a33594cee3f2fbe9f378a7b16c7d943
Kortney Wright <mithreegals1@yahoo.com>
2026-04-03 16:35:34 -0400

    added materials, added max item rules, gave items viewable names

--- Assets/Scripts/Items/ItemComponent.cs
+++ Assets/Scripts/Items/ItemComponent.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 namespace Assets.Scripts.Items
 {
-	public class ItemComponent: MonoBehaviour
-	{
+    public class ItemComponent : MonoBehaviour
+    {
         public Item itemData;
+        public TextMeshProUGUI text;
+
+        private void Start()
+        {
+            text.text = itemData.displayName;
+        }
     }
 }
